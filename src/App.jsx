3d2bb12185ebcfafa9cac1fab93c0ac72d4d2adc
Tran Huy Hoang <tranhuyhoangstu@gmail.com>
2026-03-15 22:33:30 +0700

    feat: add ParallaxHero with GSAP for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import UsersPage from './pages/UsersPage.jsx'
+import ParallaxHero from './components/ParallaxHero';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/users" replace />} />
+      <Route path="/" element={<ParallaxHero />} />
       <Route path="/users" element={<UsersPage />} />
     </Routes>
   )
